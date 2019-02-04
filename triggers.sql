b5fefdd1e5be8a43daf708d2933d6f526561f3db
Tushar <1605070@ugrad.cse.buet.ac.bd>
2019-02-04 23:07:22 +0600

    deleted commented codes

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,11 @@
+create or replace function insert_pk_to_offer() returns trigger as $$
+begin
+  insert into offers values (new.offer_id, new.price, new.validity, new.reward_points);
+  return new;
+end;
+$$ language plpgsql;
+
+
 
 drop trigger if exists insert_internet_pk_to_parent on internet_offer;
 create trigger insert_internet_pk_to_parent after insert on internet_offer
@@ -25,26 +33,3 @@ create trigger insert_general_pk_to_parent after insert on general_offer
 for each row
   execute procedure insert_pk_to_offer();
 
-
-create or replace function insert_pk_to_offer() returns trigger as $$
-begin
-  insert into offers values (new.offer_id, new.price, new.validity, new.reward_points);
-  return new;
-end;
-$$ language plpgsql;
-
-
-
-/*CREATE TRIGGER PURCHASE_TRIGGER AFTER INSERT ON OFFERS
-  FOR EACH ROW
-  EXECUTE PROCEDURE INSERT_PURCHASE_OFFER_ROW()
-*/
-/*CREATE OR REPLACE FUNCTION INSERT_PURCHASE_OFFER_ROW(IN MOB NUMERIC, IN OFFER_NO NUMERIC, IN PURCHASED_TIME TIMESTAMPTZ)
-  RETURNS TRIGGER AS $$
-DECLARE
-BEGIN
-  INSERT INTO PURCHASE_OFFER VALUES(MOB, OFFER_NO, PURCHASED_TIME);
-end;
-$$ language plpgsql;
-
-  select * from star;*/
